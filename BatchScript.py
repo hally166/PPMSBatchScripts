bab6a32d27e34040ad2e23217d3467800f06283d
Christopher <christopher.hall@sanger.ac.uk>
2018-02-08 16:46:24 +0000

    added description

--- BatchScript.py
+++ BatchScript.py
@@ -1,9 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 12 15:17:50 2017
+#Copyright (c) 2018 Genome Research Ltd.
 
-@author: ch15
-"""
+#PPMSBatchBooking
+#v1.0 Feb 2018
+#Python 2.7 (2016)
+#Author : Christopher Hall, Wellcome Sanger Institute, christopher.hall@sanger.ac.uk
+
+#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#Add your details and run this script.  It will take the details from the csv and book onto PPMS. 
 
 import requests
 import csv
