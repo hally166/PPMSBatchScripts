287888e52ca12ed08e30d67bd30ceb47885abf65
Sanger Institute Flow Cytometry <ch15@sanger.ac.uk>
2018-03-28 15:38:43 +0100

    Shortened Pre-amble

--- BatchScript.py
+++ BatchScript.py
@@ -1,13 +1,8 @@
-#Copyright (c) 2018 Genome Research Ltd.
-
 #PPMSBatchBooking
 #v1.0 Feb 2018
 #Python 2.7 (2016)
 #Author : Christopher Hall, Wellcome Sanger Institute, christopher.hall@sanger.ac.uk
-
-#This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-#This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+#License : GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
 
 #Add your details and run this script.  It will take the details from the csv and book onto PPMS. 
 
